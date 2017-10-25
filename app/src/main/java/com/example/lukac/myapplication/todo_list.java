cba163e791369fb95c16dce0adb63bc640bc305a
luca home <development.prisma@gmail.com>
2017-10-25 13:51:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lukac/myapplication/todo_list.java
@@ -0,0 +1,13 @@
+package com.example.lukac.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class todo_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_list);
+    }
+}
