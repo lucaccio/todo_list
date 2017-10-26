2a77bdac929e66f613315da7886851159ea62f75
luca <developer.luca@gmail.com>
2017-10-26 15:25:10 +0200

    inserimento toolbar + menu

--- app/src/main/java/com/example/lukac/myapplication/todo_list.java
+++ app/src/main/java/com/example/lukac/myapplication/todo_list.java
@@ -2,17 +2,36 @@ package com.example.lukac.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.Menu;
 public class todo_list extends AppCompatActivity {
+
+
+    private Toolbar t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list);
+
+         Toolbar t = (Toolbar) findViewById(R.id.toolbar);
+         // setto la Toolbar come una ActionBar
+         setSupportActionBar(t);
+        //t.inflateMenu(R.menu.main_menu);
     }
 
-    protected void onDestroy() {
-        super.onDestroy();
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        return true;
     }
 
 
