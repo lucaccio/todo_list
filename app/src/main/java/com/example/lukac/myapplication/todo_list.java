07b93a3563eebea75e9f4ad9f1c1886621944070
luca <developer.luca@gmail.com>
2017-10-27 11:53:05 +0200

    ...

--- app/src/main/java/com/example/lukac/myapplication/todo_list.java
+++ app/src/main/java/com/example/lukac/myapplication/todo_list.java
@@ -1,10 +1,16 @@
 package com.example.lukac.myapplication;
 
+import android.content.Context;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
 public class todo_list extends AppCompatActivity {
 
 
@@ -13,7 +19,24 @@ public class todo_list extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_todo_list);
+
+        RelativeLayout rl = new RelativeLayout(this);
+
+       // setContentView(R.layout.activity_todo_list);
+        final AlertDialog.Builder ad = new AlertDialog.Builder(this);
+
+        Button b = new Button(this);
+        b.setText("Close");
+        b.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                ad.setMessage("Click").show();
+            }
+        });
+        rl.addView(b);
+        setContentView(rl);
 
          Toolbar t = (Toolbar) findViewById(R.id.toolbar);
          // setto la Toolbar come una ActionBar
