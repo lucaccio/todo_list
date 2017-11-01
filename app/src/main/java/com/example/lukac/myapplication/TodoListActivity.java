6da4f0de0da855a08ec4e4022792ad1e3e333d25
luca home <development.prisma@gmail.com>
2017-11-01 21:24:01 +0100

    menu

--- app/src/main/java/com/example/lukac/myapplication/TodoListActivity.java
+++ app/src/main/java/com/example/lukac/myapplication/TodoListActivity.java
@@ -4,7 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.ContactsContract;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,6 +26,8 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 import android.net.Uri;
 
+import com.example.lukac.myapplication.database.DatabaseOpenHelper;
+
 import java.util.ArrayList;
 
 public class TodoListActivity extends AppCompatActivity {
@@ -32,71 +39,81 @@ public class TodoListActivity extends AppCompatActivity {
 
     @Override
     protected final void onCreate(Bundle savedInstanceState) {
-
-        //creo una mappa per salvare i dati
-        final SharedPreferences   pref = getSharedPreferences("TODO_PREF", Context.MODE_PRIVATE);
-
         super.onCreate(savedInstanceState);
+
+        /** alori da inserire nella lista dei todo */
+        String[] values = new String[]{};
+        ArrayList<String> al = new ArrayList<>();
+        try {
+            DatabaseOpenHelper dbh = new DatabaseOpenHelper(getApplicationContext());
+            SQLiteDatabase  db = dbh.getReadableDatabase();
+            Cursor c = db.rawQuery("SELECT * FROM todo", null);
+
+            if(c.getCount() == 0)
+            {
+                Log.d("Database Record: ", "No records found");
+                Toast mToast = Toast.makeText(this, "No record found", Toast.LENGTH_LONG);
+                mToast.show();
+
+            } else {
+                while(c.moveToNext())
+                {
+                    al.add( c.getString(1));
+                    //String title =  c.getString(1);
+                }
+                Log.d("TodoActivity", "Attempting to create " + dbh.getDatabaseName());
+                final ListView listview = (ListView) findViewById(R.id.list_view);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, al);
+                try {
+                    listview.setAdapter(adapter);
+                    listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                        @Override
+                        public void onItemClick(AdapterView<?> adattatore, final View componente, int pos, long id) {
+                            Log.d("ListView", "hai selezionato la riga " + pos);
+
+                            //creo una mappa per salvare i dati
+                            final SharedPreferences pref = getSharedPreferences("TODO_PREF", Context.MODE_PRIVATE);
+                            SharedPreferences.Editor editor = pref.edit();
+                            editor.putString("prova", "prova eseguita con successo");
+                            editor.apply();
+
+                            Intent i = new Intent(getApplicationContext(), NewActivity.class);
+                            i.putExtra("id", id);
+                            startActivity(i);
+
+
+                        }
+                    });
+                } catch (Exception e) {
+                    Toast mToast = Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT);
+                    mToast.show();
+                }
+            }
+        } catch (Exception e) {
+            Log.d("Error", e.getMessage());
+        }
         setContentView(R.layout.activity_todo_list);
 
-        /**
-         * Creo la lista delle citta
-         */
-        final ListView listview = (ListView) findViewById(R.id.list_view);
-        String[] values = new String[]{"Cagliari", "Sassari", "Nuoro", "Oristano"};
-        ArrayList<String> al = new ArrayList<String>();
-        for (int x = 0; x < values.length; ++x) {
-            al.add(values[x]);
-        }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, al);
-        try {
-            listview.setAdapter(adapter);
-        } catch (Exception e) {
-            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT);
-        }
-/*
 
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> adattatore, final View componente, int pos, long id) {
-                // recupero il titolo memorizzato nella riga tramite l'ArrayAdapter
-                final String titoloriga = (String) adattatore.getItemAtPosition(pos);
-                Log.d("List", "Ho cliccato sull'elemento con titolo " + titoloriga);
-                Log.d("List", "Ho cliccato sull'elemento con id " + id);
-
-                Intent i = new Intent(getApplicationContext(), DetailsActivity.class);
-                i.putExtra("Città", titoloriga);
-                i.putExtra("Id", id);
-                startActivity(i);
-
-            }
-        });
-*/
-
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
+        FloatingActionButton add = (FloatingActionButton) findViewById(R.id.add_btn);
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adattatore, final View componente, int pos, long id) {
-                Log.d("ListView", "hai selezionato la riga " + pos);
-
-
-                SharedPreferences.Editor editor = pref.edit();
-                editor.putString("prova", "prova eseguita con successo");
-                editor.apply();
-
-                Intent i = new Intent(getApplicationContext(), NewActivity.class);
-                i.putExtra("id", id);
-                startActivity(i);
-
-
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
+
+
+
 
 
     }
 
+
+
     public boolean getIntenti() {
-
-
         startActivityForResult(
                 new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI),
                 PICK_CONTACT_REQUEST);
---
+++ app/src/main/java/com/example/lukac/myapplication/database/DatabaseOpenHelper.java
@@ -0,0 +1,42 @@
+package com.example.lukac.myapplication.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by lukac on 01/11/2017.
+ */
+
+public class DatabaseOpenHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "todo_app_example.db";
+    private static final String DATABASE_TABLE = "todo";
+    private static final String CREATE_DB =   "CREATE TABLE " + DATABASE_TABLE + " (" +
+            "`id`	INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "`title`    TEXT," +
+            "`desciption` TEXT," +
+            "`date`	NUMERIC "  + ")" ;
+
+    public DatabaseOpenHelper(Context context) {
+        super(context, DATABASE_NAME ,null, DATABASE_VERSION);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_DB);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public String getDatabaseName() {
+        return DATABASE_NAME;
+    }
+
+
+}
